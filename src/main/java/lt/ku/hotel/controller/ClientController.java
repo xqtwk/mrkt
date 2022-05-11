cab70bdeeff198903103ee2307ac1d5d37a62d30
Richard <xqtwkk@gmail.com>
2022-05-11 23:48:35 +0300

    feat: authorization implemented

---
+++ src/main/java/lt/ku/hotel/controller/ClientController.java
@@ -0,0 +1,9 @@
+package lt.ku.hotel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/clients")
+public class ClientController {
+}

d44fbfac3b38585b192c6fea75afbb12a13a4b04
Lukas Brizgys <lukenis456@gmail.com>
2022-05-15 22:53:31 +0300

    add additional method to repository

--- src/main/java/lt/ku/hotel/repositories/RoomRepository.java
+++ src/main/java/lt/ku/hotel/repositories/RoomRepository.java
@@ -1,9 +1,16 @@
 package lt.ku.hotel.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import lt.ku.hotel.entities.Room;
 
+@Repository
 public interface RoomRepository extends JpaRepository<Room, Integer>{
-
+	@Query("SELECT r FROM Room r WHERE r.is_reserved = 0")
+	List<Room> findAllUnreservedRooms();
+	
 }

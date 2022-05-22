1aec24e28a89b2d6742e2ea8294481b51322ed36
Richard <xqtwkk@gmail.com>
2022-05-22 14:43:55 +0300

    finished reservation price js script

--- src/main/resources/static/js/reservation.js
+++ src/main/resources/static/js/reservation.js
@@ -1,11 +1,16 @@
 const form = document.getElementsByTagName("form")[0];
 
 const services = document.getElementById('servicesCheckbox');
-const meals = document.getElementById('mealsCheckbox');
+const days = document.getElementById('days').innerText.replace(/[^0-9]/g, '');
 
+const startPrice = document.querySelector(".actual-price").innerText.replace(/[^0-9.]/g, '');
+const priceElement = document.querySelector(".actual-price");
+let price = startPrice * parseFloat(days);
+(function() {
+    priceElement.innerText = `${(price).toFixed(2)}€`;
+})();
 form.addEventListener("click", (e) =>{
     const target = e.target;
-    const priceElement = document.querySelector(".actual-price");
     let price = parseFloat(priceElement.innerText);
     if(String(target).toLowerCase().includes("input")) {
         const clickedCheckboxValue = parseFloat(target.value);
@@ -13,29 +18,3 @@ form.addEventListener("click", (e) =>{
             : `${(price - clickedCheckboxValue).toFixed(2)}€`;
     }
 })
-/*
-booking.meals.addEventListener("change", printF1func);
-booking.services.addEventListener("change", printF2func);
-var printF1 = document.getElementById('total');
-function printF1func() {
-    if (booking.meals.checked) {
-        if (isNaN(parseFloat(printF1.innerHTML))){
-            printF1.innerHTML += booking.meals.value;
-        } else {
-            printF1.innerHTML += parseFloat(printF1.innerHTML) + booking.meals.value;
-        }
-} else {                                                                   // template for all
-        if (isNaN(parseFloat(printF1.innerHTML))){
-            printF1.innerHTML -= printF1.innerHTML;
-        } else {
-            printF1.innerHTML -= parseFloat(printF1.innerHTML) - booking.meals.value;
-        }
-    }
-};
-function printF2func() {
-    if (booking.services.checked) {
-        printF1.innerHTML += booking.services.value;
-    } else {
-        printF1.innerHTML -= booking.services.value;;
-    }
-};*/

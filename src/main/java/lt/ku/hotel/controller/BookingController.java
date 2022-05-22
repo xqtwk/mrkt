6cf1397222d83e677804e165da69346e15d964d0
Lukas Brizgys <lukenis456@gmail.com>
2022-05-22 11:20:36 +0300

    minor change in booking controller

--- src/main/java/lt/ku/hotel/controller/BookingController.java
+++ src/main/java/lt/ku/hotel/controller/BookingController.java
@@ -1,35 +1,52 @@
 package lt.ku.hotel.controller;
 
-import lt.ku.hotel.repositories.BookingRepository;
+import lt.ku.hotel.entities.Room;
 import lt.ku.hotel.services.BookingService;
 import lt.ku.hotel.services.RoomService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.text.DateFormat;
-import java.text.ParseException;
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.time.temporal.ChronoUnit;
-import java.util.Date;
 
 @Controller
 public class BookingController {
-	private BookingRepository bookingRepository;
-	private RoomService roomService;
+	@Autowired
+	RoomService roomService;
+	
+	
+	@Autowired
+	BookingService bookingService;
 	//@RequestMapping(value="room", method = RequestMethod.GET)
 	@GetMapping("/reserve")
 	public String getReservation(Model model,
-								 @RequestParam(required = false, value = "arrivalDate") String arrival,
-								 @RequestParam(required = false, value = "departureDate") String departure,
-								 @RequestParam(required = false, value = "guestCount") String guestCount) throws ParseException {
-		model.addAttribute("arrivalDate", arrival);
-		model.addAttribute("departureDate", departure);
-		model.addAttribute("guestCount", guestCount);
-		long noOfDaysBetween = ChronoUnit.DAYS.between(LocalDate.parse(arrival), LocalDate.parse(departure));
-		//System.out.println(noOfDaysBetween);
-		model.addAttribute("dayCount", noOfDaysBetween);
-		return "reservation";
+			@RequestParam(required = false, value = "roomId") Integer roomId,
+			@RequestParam(required = false, value = "arrivalDate") String arrival,
+			@RequestParam(required = false, value = "departureDate") String departure,
+			@RequestParam(required = false, value = "guestCount") Integer guestCount){
+		try {
+			if(roomId == null || arrival == null || departure == null || guestCount == null) {
+				return "redirect:/";
+				}
+			boolean isReserved = roomService.isRoomReserved(roomId, arrival, departure, guestCount);
+			if(isReserved) return "redirect:/";
+			Room room = roomService.getRoom(roomId);
+			model.addAttribute("roomPrice", room.getPrice());
+			model.addAttribute("arrivalDate", arrival);
+			model.addAttribute("departureDate", departure);
+			model.addAttribute("guestCount", guestCount);
+			long noOfDaysBetween = ChronoUnit.DAYS.between(LocalDate.parse(arrival), LocalDate.parse(departure));
+			model.addAttribute("dayCount", noOfDaysBetween);
+			
+			return "reservation";
+		}catch(DateTimeParseException e) {
+			return "redirect:/";
+		}
+		
 	}
 	@PostMapping("/reserve")
 	public String addReservation(Model model) {
--- src/main/java/lt/ku/hotel/services/BookingService.java
+++ src/main/java/lt/ku/hotel/services/BookingService.java
@@ -1,4 +1,7 @@
 package lt.ku.hotel.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookingService {
 }

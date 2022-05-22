834139ea85962312b563f23fde298f87f8c08947
Richard <xqtwkk@gmail.com>
2022-05-22 03:15:25 +0300

    move reservation price count script to separate file

---
+++ src/main/resources/static/js/reservation.js
@@ -0,0 +1,17 @@
+const form = document.getElementsByTagName("form")[0];
+
+
+const services = document.getElementById('servicesCheckbox');
+const meals = document.getElementById('mealsCheckbox');
+
+
+form.addEventListener("click", (e) =>{
+    const target = e.target;
+    const priceElement = document.querySelector(".actual-price");
+    let price = parseFloat(priceElement.innerText);
+    if(String(target).toLowerCase().includes("input")){
+        const clickedCheckboxValue = parseFloat(target.value);
+        priceElement.innerText = target.checked ? `${(price + clickedCheckboxValue).toFixed(2)}€`
+            : `${(price - clickedCheckboxValue).toFixed(2)}€`;
+    }
+})
